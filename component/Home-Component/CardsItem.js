66ab82dfb6b90fcd0dfd1404b4bf24c1ac65f9b9
Eslam Aboudesheesh <t-emhussein@efg-hermes.com>
2020-12-24 01:21:06 +0200

    add overlay for cards

--- component/Home-Component/CardsItem.js
+++ component/Home-Component/CardsItem.js
@@ -11,6 +11,10 @@ export default function CardsItem() {
           <small>Tillandsia cyanea Pink</small>
 
         </Card.Footer>
+        <div className="card-overlay">
+          <a className="btn btn-defualt" href="#"><i className="fa fa-heart"></i> </a> 
+          <a className="btn btn-defualt" href="#"> <i className="fa fa-eye"></i> </a>
+        </div>
       </Card>
     </>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.scss';
 import {Provider} from 'react-redux';
 import store from '../redux/PlantsStore';
+import 'font-awesome/css/font-awesome.min.css';
 
 function MyApp({ Component, pageProps }) {
   return ( 
