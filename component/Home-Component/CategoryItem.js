be89b45b397b7e32f7e327c3c820b5e8f05421fb
Eslam Aboudesheesh <t-emhussein@efg-hermes.com>
2020-12-24 03:39:07 +0200

    fixed css  our productes

--- component/Home-Component/CategoryItem.js
+++ component/Home-Component/CategoryItem.js
@@ -4,7 +4,7 @@ import { Card, Button } from 'react-bootstrap'
 export default function CategoryItem({ title }) {
   return (
     <>
-      <Card className=" col-md-3 ">
+      <Card className=" col-md-3 Category__Card ">
         <Card.Img variant="top" src="/images/Pots.jpg" alt="plant img " />
         <Card.ImgOverlay>
            <button className="btn Main__btn">
